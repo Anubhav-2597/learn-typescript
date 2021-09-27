6d81bff02d297c70e5cae558c691aa67d5a80382
Anubhav-c9 <anubhavajmera25@gmail.com>
2021-09-27 13:40:13 +0000

    learn abour conditionals

---
+++ conditionals.ts
@@ -0,0 +1,9 @@
+var password : string = 'zcnvmcn';
+
+if(password == 'asdjfp'){
+    console.log("yes, asdjfp is the password");
+}else if(password == 'zcnvmcn'){
+    console.log("yes, zcnvmcn is the password");
+}else{
+    console.log("sorry, permission denied");
+}
