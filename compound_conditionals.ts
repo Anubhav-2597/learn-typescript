13db2b5ca717f5595e968722a2efe48936d3b2a4
Anubhav-c9 <anubhavajmera25@gmail.com>
2021-09-27 14:15:23 +0000

    learnt about compound conditional operators

---
+++ compound_conditionals.ts
@@ -0,0 +1,12 @@
+let password : string = 'xbcvn';
+let email : string = 'abc12@gmail.com'
+
+// if (password == 'asdfgh' || password == 'xbcvn'){
+//     console.log("you are authorized");
+// }else{
+//     console.log("permission denied");
+// }
+
+if (!(email == 'abc12@gmail.com') ){
+    console.log("you are authorized");
+}
