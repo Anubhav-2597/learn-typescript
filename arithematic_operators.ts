78d4d7926ad874ed3ad5ad960ca2399311c9d539
Anubhav-c9 <anubhavajmera25@gmail.com>
2021-09-27 13:25:31 +0000

    learnt about arithematic operators

---
+++ arithematic_operators.ts
@@ -0,0 +1,44 @@
+var numOne : number = 1;
+var numTwo : number = 2;
+
+//Addition
+console.log("Addition: ");
+console.log(numOne + numTwo);
+
+//substraction
+console.log("substraction: ");
+console.log(numTwo - numOne);
+
+//multiplication
+numOne = 10;
+numTwo = 15;
+console.log("multiplication: ");
+console.log(numOne*numTwo);
+
+//division
+console.log("division: ");
+console.log(numOne / numTwo);
+
+//Modulus
+console.log("modulus: ");
+var numThree : number = 2;
+var numFour : number = 20;
+console.log(numFour % numThree);
+
+//incrementor
+console.log("incrementor: ");
+var x : number = 0;
+
+while(x < 10){
+    console.log(x);
+    x++;
+}
+
+//decrementor
+console.log("decrementor: ");
+var x : number = 10;
+
+while(x > 0){
+    console.log(x);
+    x--;
+}
