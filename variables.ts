ce7a4d9f541cb0e692a08963d8326e7c87ce7291
Anubhav-c9 <anubhavajmera25@gmail.com>
2021-09-27 08:49:31 +0000

    changes done to variables.ts

--- variables.ts
+++ variables.ts
@@ -2,6 +2,9 @@ var fullName : string = "anubhav";
 let paidAccount : boolean = true;
 const versionNumber : number = 1.3;
 
+fullName = 'ajmera'
+paidAccount = false
+
 console.log(fullName);
 console.log(paidAccount);
 console.log(versionNumber);
